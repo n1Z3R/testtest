a4643f055a06fd9264966db2f851ad16a8c8eaf5
n1Z3R <dan3994@yandex.ru>
2020-07-05 18:32:45 +0300

    Test

---
+++ test/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lst = new System.Windows.Forms.ListView();
+            this.id = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.type = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.bid = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cbid = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.available = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.url = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // lst
+            // 
+            this.lst.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.id,
+            this.type,
+            this.bid,
+            this.cbid,
+            this.available,
+            this.url});
+            this.lst.HideSelection = false;
+            this.lst.Location = new System.Drawing.Point(12, 12);
+            this.lst.Name = "lst";
+            this.lst.Size = new System.Drawing.Size(1379, 426);
+            this.lst.TabIndex = 0;
+            this.lst.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1403, 450);
+            this.Controls.Add(this.lst);
+            this.Name = "Form1";
+            this.Text = "Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lst;
+        private System.Windows.Forms.ColumnHeader id;
+        private System.Windows.Forms.ColumnHeader type;
+        private System.Windows.Forms.ColumnHeader bid;
+        private System.Windows.Forms.ColumnHeader cbid;
+        private System.Windows.Forms.ColumnHeader available;
+        private System.Windows.Forms.ColumnHeader url;
+    }
+}
+
---
+++ test/Form1.cs
@@ -0,0 +1,61 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        public static MySqlConnection connection;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+     
+        MySqlCommand cmd = new MySqlCommand();
+        MySqlDataReader dr;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string server = "localhost";
+            string database = "test";
+            string uid = "root";
+            string password = "admin";
+            string connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+            MySqlConnection cn = new MySqlConnection(connectionString);
+            cn.Open();
+            cmd.CommandText = "select * from yml_last";
+            cmd.Connection = cn;
+            dr = cmd.ExecuteReader();
+
+            lst.Columns.Clear();
+            lst.Items.Clear();
+            lst.View = View.Details;
+
+            for (int i = 0; i < dr.FieldCount; i++)
+            {
+                lst.Columns.Add(dr.GetName(i));
+            }
+            while (dr.Read())
+            {
+                ListViewItem lv = new ListViewItem(dr[0].ToString());
+                for (int i = 0; i < dr.FieldCount; i++)
+                {
+                    lv.SubItems.Add(dr[i].ToString());
+                }
+                lst.Items.Add(lv);
+            }
+            cn.Close();
+        }
+        
+    }
+}
+        
